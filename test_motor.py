f67682fa940dac168735e111b8f321e8b147ad09
Sanduni <sanduacademic@gmail.com>
2020-08-24 20:58:04 +0530

    movement class

--- test_motor.py
+++ test_motor.py
@@ -35,7 +35,55 @@ q.start(0)
 
 
 #while True:
-    
+class Movement(object):
+	"""docstring for Movement"""
+	def __init__(self, LPWM, RPWM):
+		super(Movement, self).__init__()
+		self.LPWM = LPWM
+		self.RPWM = RPWM
+
+	def turnLeft(speed):
+		GPIO.output(RB, GPIO.LOW)
+		GPIO.output(LF, GPIO.LOW)
+		GPIO.output(LB, GPIO.HIGH)
+		GPIO.output(RF, GPIO.HIGH)
+
+		LPWM.ChangeDutyCycle(speed)
+		RPWM.ChangeDutyCycle(speed)
+
+	def turnRight(speed):
+		GPIO.output(RB, GPIO.HIGH)
+		GPIO.output(LF, GPIO.HIGH)
+		GPIO.output(LB, GPIO.LOW)
+		GPIO.output(RF, GPIO.LOW)
+
+		LPWM.ChangeDutyCycle(speed)
+		RPWM.ChangeDutyCycle(speed)
+
+	def turnBack(speed):
+		GPIO.output(RB, GPIO.LOW)
+		GPIO.output(LF, GPIO.LOW)
+		GPIO.output(LB, GPIO.HIGH)
+		GPIO.output(RF, GPIO.HIGH)
+
+		LPWM.ChangeDutyCycle(speed)
+		RPWM.ChangeDutyCycle(speed)
+
+	def brake():
+		GPIO.output(LB, GPIO.HIGH)
+		GPIO.output(RF, GPIO.HIGH)
+		GPIO.output(RB, GPIO.HIGH)
+		GPIO.output(LF, GPIO.HIGH)
+
+		LPWM.ChangeDutyCycle(255)
+		RPWM.ChangeDutyCycle(255)
+
+	def stop():
+		GPIO.output(LB, GPIO.LOW)
+		GPIO.output(RF, GPIO.LOW)
+		GPIO.output(RB, GPIO.LOW)
+		GPIO.output(LF, GPIO.LOW)
+
     
     
         
